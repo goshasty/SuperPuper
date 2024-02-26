d402331aff86751eed7ae4eea32449e8f586786f
Georgii Demin <goshasty@yandex.ru>
2024-02-26 21:16:19 +0300

    hw2, n/wrap_text params is flag now

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -11,19 +11,17 @@ def main(args):
         wrap_text=args.wrap_text,
         width=args.width
     )
+    
     print(cow_said)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     
-    parser.add_argument('message', nargs='*', default='')
+    parser.add_argument('message', nargs='*', default=' ')
     parser.add_argument('-e', dest='eyes', type=str, default='oo')
     parser.add_argument('-T', dest='tongue', type=str, default='__')
     parser.add_argument('-f', dest='cowfile', type=str, default=None)
-    parser.add_argument(
-        '-n', dest='wrap_text', type=bool, default=True, 
-        help="To use -n arg: python3 cow_say.py -n '' <other args>/<message>"
-    )
+    parser.add_argument('-n', dest='wrap_text', action='store_false')
     parser.add_argument('-W', dest='width', type=int, default=40)
    
     args = parser.parse_args()
