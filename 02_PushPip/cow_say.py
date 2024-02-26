12c2d749012e1eb37c3b7c1c7778e3a28309e886
Georgii Demin <goshasty@yandex.ru>
2024-02-26 21:24:25 +0300

    hw2, mode params

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -1,15 +1,39 @@
 import argparse
 import cowsay
 
+def handle_preset(args) -> str:
+    # -bgpstwy
+    preset_list = []
+    if args.b:
+        preset_list.append('b')
+    if args.g:
+        preset_list.append('g')
+    if args.p:
+        preset_list.append('p')
+    if args.s:
+        preset_list.append('s')
+    if args.t:
+        preset_list.append('t')
+    if args.w:
+        preset_list.append('w')
+    if args.y:
+        preset_list.append('y')
+        
+    return ''.join(preset_list)
+        
+
 def main(args):
     message = ' '.join(args.message)
+    preset = handle_preset(args)
+    
     cow_said = cowsay.cowsay(
         message=message,
         eyes=args.eyes,
         tongue=args.tongue,
         cowfile=args.cowfile,
         wrap_text=args.wrap_text,
-        width=args.width
+        width=args.width,
+        preset=preset
     )
     
     print(cow_said)
@@ -23,7 +47,10 @@ if __name__ == '__main__':
     parser.add_argument('-f', dest='cowfile', type=str, default=None)
     parser.add_argument('-n', dest='wrap_text', action='store_false')
     parser.add_argument('-W', dest='width', type=int, default=40)
+    
+    modes = 'bgpstwy'
+    for mode in modes:
+        parser.add_argument(f'-{mode}', dest=f'{mode}', action='store_true')
    
     args = parser.parse_args()
-    
     main(args)
