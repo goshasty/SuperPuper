a2a2fee6f205563adfbaf7b7797719888f62e347
Georgii Demin <goshasty@yandex.ru>
2024-02-26 20:42:44 +0300

    hw2, param tongue

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -5,14 +5,17 @@ def main(args):
     message = ' '.join(args.message)
     cow_said = cowsay.cowsay(
         message=message,
-        eyes=args.eyes
+        eyes=args.eyes,
+        tongue=args.tongue
     )
     print(cow_said)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    
     parser.add_argument('message', nargs='*', default='')
     parser.add_argument('-e', '--eyes', type = str, default = 'oo')
+    parser.add_argument('-T', '--tongue', type = str, default = '__')
    
     args = parser.parse_args()
     
