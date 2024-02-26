e59885344072f20990b0e01f00ae4a44f3dd4e95
Georgii Demin <goshasty@yandex.ru>
2024-02-26 20:38:09 +0300

    hw2, param eyes

--- 02_PushPip/cow_say.py
+++ 02_PushPip/cow_say.py
@@ -3,12 +3,16 @@ import cowsay
 
 def main(args):
     message = ' '.join(args.message)
-    cow_said = cowsay.cowsay(message=message)
+    cow_said = cowsay.cowsay(
+        message=message,
+        eyes=args.eyes
+    )
     print(cow_said)
     
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('message', nargs='*', default='')
+    parser.add_argument('-e', '--eyes', type = str, default = 'oo')
    
     args = parser.parse_args()
     
