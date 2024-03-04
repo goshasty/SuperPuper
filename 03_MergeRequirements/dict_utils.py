44b15ebaadf18f266604cb9dc30b9dc9f5a4219e
Georgii Demin <goshasty@yandex.ru>
2024-03-04 22:25:26 +0300

    handle dictionary file or url

--- 03_MergeRequirements/dict_utils.py
+++ 03_MergeRequirements/dict_utils.py
@@ -1,11 +1,39 @@
 import os
 from typing import List
+import urllib
+from urllib.parse import urlparse
+import urllib.request
+from pathlib import Path
+
+
+
+def download_words(url):
+    DICT_PATH = "dictionaries/"
+    DICT_FILENAME = "words.txt"
+    
+    DICT_FULL_PATH = Path(DICT_PATH, DICT_FILENAME)
+    p = Path(DICT_PATH)
+    p.mkdir(parents=True, exist_ok=True)
+    urllib.request.urlretrieve(url, DICT_FULL_PATH)
+    
+    return DICT_FULL_PATH
+
+
+def is_valid_url(url_candidate: str) -> bool:
+    parsed_url = urlparse(url_candidate)
+    return bool(parsed_url.scheme)
+
 
 def load_dict(dictionary: str) -> List[str]:
-    is_path = os.path.exists(dictionary)
+    if is_valid_url(dictionary):
+        dictionary = download_words(dictionary)
+    is_path = Path(dictionary).exists()
     if is_path:
         with open(dictionary, 'r') as fd:
             words = fd.read().splitlines()
-    else:
-        return []
-    return words
+            return words
+    
+    raise ValueError('dictionary is nor valid url neither path')
+    
+    
+    
