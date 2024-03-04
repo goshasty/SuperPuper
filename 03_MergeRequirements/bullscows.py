74af887fc2cf12bcee0318f7da21a3d9a23f8f9d
Georgii Demin <goshasty@yandex.ru>
2024-03-04 21:40:54 +0300

    gameplay full cicle

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -18,12 +18,26 @@ def ask(prompt: str, valid: List[str] = None) -> str:
 def inform(format_string: str, bulls: int, cows: int) -> None:
     print(format_string.format(bulls, cows))
 
-def gameplay(ask: callable, inform: callable, words: List[str]) -> int:
-    secret = random.choice(words)
-    guess = ask("Введите слово: ", words)
-    bulls, cows = bullscows(guess, secret)
-    inform("Быки: {}, Коровы: {}", bulls, cows)
-    return True
+def is_guessed(bulls: int, secret: str) -> bool:
+    return bulls == len(secret)
 
-print(gameplay(ask, inform, ['полип']))
+def end_game(num_tries: int) -> None:
+     print('Поздравляю!'.format(num_tries))
+
+def gameplay(ask: callable, inform: callable, words: List[str]) -> None:
+    secret = random.choice(words)
+    guessed = False
+    num_tries = 0
+    while guessed == False:
+        num_tries += 1
+        guess = ask("Введите слово: ", words)
+        bulls, cows = bullscows(guess, secret)
+        if is_guessed(bulls, secret):
+            end_game(num_tries)
+            break
+        
+        inform("Быки: {}, Коровы: {}", bulls, cows)
+
+if __name__ == '__main__':
+    gameplay(ask, inform, ['полип'])
 
