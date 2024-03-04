b45ff6dc70a26757e76ea9b17e4d402b248cde83
Georgii Demin <goshasty@yandex.ru>
2024-03-04 21:31:35 +0300

    mvp gameplay one turn

--- 03_MergeRequirements/bullscows.py
+++ 03_MergeRequirements/bullscows.py
@@ -1,4 +1,5 @@
-from typing import Tuple
+from typing import Tuple, List
+import random
 
 def bullscows(guess: str, secret: str) -> Tuple[int, int]:
     bulls = 0
@@ -10,5 +11,18 @@ def bullscows(guess: str, secret: str) -> Tuple[int, int]:
         cows += guess_letter in secret
     return (bulls, cows-bulls)
 
-#print(bullscows("ропот", "полип"))
+def ask(prompt: str, valid: List[str] = None) -> str:
+    return 'ропот'
+
+def inform(format_string: str, bulls: int, cows: int) -> None:
+    print(format_string.format(bulls, cows))
+
+def gameplay(ask: callable, inform: callable, words: List[str]) -> int:
+    secret = random.choice(words)
+    guess = ask("Введите слово: ", words)
+    bulls, cows = bullscows(guess, secret)
+    inform("Быки: {}, Коровы: {}", bulls, cows)
+    return True
+
+print(gameplay(ask, inform, ['полип']))
 
