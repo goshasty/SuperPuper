a682b457cc7e22f8b7a89c9b866851dab0f84200
goshasty <goshasty@yandex.ru>
2024-03-11 20:21:50 +0000

    complete cpwsay

--- 04_MergetoolCommandline/main.py
+++ 04_MergetoolCommandline/main.py
@@ -1,8 +1,12 @@
 import shlex
 import cmd
-from cowsay import list_cows, cowsay
-from itertools import batched
+from cowsay import list_cows, cowsay, Option
+#from itertools import batched
 
+def batched(args, n):
+    if n==2:
+        return [(args[i*2], args[i*2+1]) for i in range(0, (len(args)+1)//2)]
+    
 class Cow(cmd.Cmd):
     prompt = "<<^_^>> "
     d_params_cowsay = {
@@ -10,9 +14,16 @@ class Cow(cmd.Cmd):
         '-t': 'tongue',
         '-f': 'cow',
     }
-
+    
+    d_default_cowsay = {
+        '-e': ['oo', '$$'],
+        '-t': ['=', '[['],
+        '-f': list_cows()[:4],
+        
+    }
     def __cowsay_params(self, args):
         params = {}
+        print(batched(args, 2))
         for arg, val in batched(args, 2):
             params[self.d_params_cowsay[arg]] = val
         return params
@@ -27,6 +38,12 @@ class Cow(cmd.Cmd):
         args = shlex.split(args)
         params=self.__cowsay_params(args[1:])
         print(cowsay(args[0], **params))
+    
+    def complete_cowsay(self, text, line, begidx, endidx) -> list[str] | None:
+        words = (line[:endidx] + " ").split()
+        return self.d_default_cowsay.get(words[-1], None)
+        return words
+        return [str(text), str(words), str(line), str(begidx), str(endidx)]
         
     def do_EOF(self, args=None):
         return 1
